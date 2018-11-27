c386254a0f514156da0f6f94e50c678d49fd540d
Phil Griffiths <phil@redhat.com>
2018-11-27 15:44:20 +0000

    new

---
+++ tower/library/scan_foo.py
@@ -0,0 +1,16 @@
+def main():
+    module = AnsibleModule(
+        argument_spec = dict())
+
+    foo = [
+      {
+        "hello": "world"
+      },
+      {
+        "foo": "bar"
+      }
+    ]
+    results = dict(ansible_facts=dict(foo=foo))
+    module.exit_json(**results)
+
+main()
